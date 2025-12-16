04bdbf3fef8a1a90e2081138e116b3728cb1c7ea
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2025-12-15 18:07:17 -0600

    it should correctly apply the eventloop without needing to create a new one each time (by clearing it)

--- src/main/java/frc/robot/controls/ControllerMappings.java
+++ src/main/java/frc/robot/controls/ControllerMappings.java
@@ -1,14 +1,13 @@
 package frc.robot.controls;
 
-import badgerutils.triggers.RobotTriggers;
 import com.ctre.phoenix6.SignalLogger;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
@@ -31,47 +30,42 @@ public class ControllerMappings {
     }
 
     public void bindDefaultControls() {
-        EventLoop eventLoop = new EventLoop();
         clearAllPreviousControls();
-        bindCommonControls(eventLoop);
+        bindCommonControls();
 
         drivetrain.setDefaultCommand(controls.getDrivetrainFieldCentricCommand());
 
-        driverController.leftBumper(eventLoop).onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
-        driverController.a(eventLoop).whileTrue(drivetrain.applyRequest(() -> controls.brake));
-
-        CommandScheduler.getInstance().setActiveButtonLoop(eventLoop);
+        driverController.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+        driverController.a().whileTrue(drivetrain.applyRequest(() -> controls.brake));
     }
 
     public void bindSwerveTestingControls() {
-        EventLoop eventLoop = new EventLoop();
         clearAllPreviousControls();
-        bindCommonControls(eventLoop);
+        bindCommonControls();
 
         drivetrain.setDefaultCommand(controls.getDrivetrainFieldCentricCommand());
         
-        driverController.a(eventLoop).whileTrue(drivetrain.applyRequest(() -> controls.brake));
-        driverController.b(eventLoop).whileTrue(drivetrain.applyRequest(() ->
+        driverController.a().whileTrue(drivetrain.applyRequest(() -> controls.brake));
+        driverController.b().whileTrue(drivetrain.applyRequest(() ->
                 controls.point.withModuleDirection(new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))
         ));
 
-        driverController.start(eventLoop).and(driverController.a(eventLoop)).onTrue(new InstantCommand(SignalLogger::start));
-        driverController.back(eventLoop).and(driverController.y(eventLoop)).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        driverController.back(eventLoop).and(driverController.x(eventLoop)).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        driverController.start(eventLoop).and(driverController.y(eventLoop)).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        driverController.start(eventLoop).and(driverController.x(eventLoop)).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
-        driverController.start(eventLoop).and(driverController.b(eventLoop)).onTrue(new InstantCommand(SignalLogger::stop));
+        driverController.start().and(driverController.a()).onTrue(new InstantCommand(SignalLogger::start));
+        driverController.back().and(driverController.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        driverController.back().and(driverController.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        driverController.start().and(driverController.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        driverController.start().and(driverController.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        driverController.start().and(driverController.b()).onTrue(new InstantCommand(SignalLogger::stop));
 
-        driverController.leftBumper(eventLoop).onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
-
-        CommandScheduler.getInstance().setActiveButtonLoop(eventLoop);
+        driverController.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
     }
 
-    public void bindCommonControls(EventLoop eventLoop) {
-        RobotTriggers.disabled(eventLoop).whileTrue(drivetrain.applyRequest(() -> controls.idle).ignoringDisable(true));
+    public void bindCommonControls() {
+        RobotModeTriggers.disabled().whileTrue(drivetrain.applyRequest(() -> controls.idle).ignoringDisable(true));
     }
 
     public void clearAllPreviousControls(){
+        CommandScheduler.getInstance().getActiveButtonLoop().clear();
         removeAndCancelDefaultCommand(drivetrain);
     }
 
